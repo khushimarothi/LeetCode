e2afcc154bf8ff6b94d85da7054cc524d89cc09a
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2022-10-12 12:15:57 +0530

    Create matrix-diagonal-sum.cpp

---
+++ matrix-diagonal-sum.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/matrix-diagonal-sum/
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int sum=0,i=0,j=0;
+        while(i<n){
+            sum+=mat[i][j];
+            i++; j++;
+        }
+        i=0;
+        j=n-1;
+        while(i<n){
+            if(i!=j)
+                sum+=mat[i][j];
+            i++;j--;
+            
+        }
+        return sum;
+    }
+};
