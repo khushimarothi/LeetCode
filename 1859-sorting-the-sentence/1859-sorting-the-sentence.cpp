f3d9fea253bbd84c10daaa6a9d08fe45d1232d22
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2023-04-08 23:34:28 +0530

    Time: 4 ms (37.13%), Space: 6.3 MB (78.15%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        string word;
+        vector<string> vec(10);
+        for(int i=0;i<s.size(); i++){
+          if(s[i]>=48 && s[i]<=57){
+              vec[s[i]-48]=word+" ";
+              word ="";
+              i++;
+          }
+           else word+=s[i];
+            
+        }
+        string ans;
+        for(string it: vec){
+            ans+=it;
+        }
+        ans.pop_back();
+        return ans;
+        
+    }
+};
