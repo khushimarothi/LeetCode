1728aed2f381071ab8b6fe0bef210c9d094c11d9
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2023-04-09 16:47:19 +0530

    Create prime-in-diagonal.cpp

---
+++ prime-in-diagonal.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/prime-in-diagonal/
+
+class Solution {
+public:
+    bool isPrime(int n) {
+            if (n<=1) 
+                return false;
+        
+            for(int i=2; i<=sqrt(n);i++) {
+               if (n%i == 0) 
+                  return false;
+   
+            }
+      return true;
+  }
+    
+    int diagonalPrime(vector<vector<int>>& nums) {
+     
+          int n=nums.size();
+          int maxi=0;
+          for (int i=0;i<n;i++) {
+              if(isPrime(nums[i][i])) 
+                  maxi= max(maxi, nums[i][i]);
+              
+              if(isPrime(nums[i][n-1-i])) 
+                  maxi= max(maxi, nums[i][n - i - 1]);
+    }
+   
+        return maxi;
+    }
+};
