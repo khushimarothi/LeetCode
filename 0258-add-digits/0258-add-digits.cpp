c4eebe0704a0770bcacc879c9f64dab456c5698b
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2023-04-26 11:24:48 +0530

    Time: 7 ms (16.24%), Space: 6 MB (7.98%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        int res=0;
+        
+        while(num>0 || res>9){
+             if(num==0){
+                num=res;
+                res=0;
+            }
+            
+            res+=num%10;
+            num/=10;
+            
+           
+            
+        }
+        return res;
+    }
+};
