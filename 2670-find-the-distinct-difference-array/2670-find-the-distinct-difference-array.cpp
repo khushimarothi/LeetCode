3a70774ab0452b25ad67173da9f3ea9607cad6d5
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2023-05-07 18:58:11 +0530

    Time: 192 ms (42.86%), Space: 110.9 MB (42.86%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> distinctDifferenceArray(vector<int>& nums) {
         int n = nums.size();
        
+        vector<int> diff(n);
+    
+        for (int i = 0; i < n; i++) {
         
-    vector<int> diff(n);
-
-    for (int i = 0; i < n; i++) {
-        unordered_set<int> prefix(nums.begin(), nums.begin() + i + 1);
-        unordered_set<int> suffix(nums.begin() + i + 1, nums.end());
-
-        diff[i] = prefix.size() - suffix.size();
+            unordered_set<int> prefix(nums.begin(), nums.begin() + i + 1);
+            unordered_set<int> suffix(nums.begin() + i + 1, nums.end());
+          
+            diff[i] = prefix.size() - suffix.size();
     }
 
     return diff;
