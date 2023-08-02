45ebc23284f3b75039241819c8f04f4dd9365822
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2023-08-02 23:48:20 +0530

    Create permutation.cpp

---
+++ Permutation/permutation.cpp
@@ -0,0 +1,21 @@
+//link : https://leetcode.com/problems/permutations/description/
+class Solution {
+    void permutation(vector<int>& nums,int i,int &numsize, vector<vector<int>> &ans){
+        if(i==numsize){
+            ans.push_back(nums);
+            
+        }
+        for(int j=i;j<numsize;j++){
+            swap(nums[i],nums[j]);
+            permutation(nums,i+1,numsize, ans);
+            swap(nums[i],nums[j]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int numsize= nums.size();
+        permutation(nums,0,numsize,ans);
+        return ans;
+    }
+};
