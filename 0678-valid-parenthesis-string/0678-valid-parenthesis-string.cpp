eb2ffe5a0d0ef81c9ea0ec68bd9968d21052054f
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2024-04-07 22:16:33 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (27.25%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -2,27 +2,35 @@ class Solution {
 public:
     bool checkValidString(string s) {
         
-      int hi = 0, lo = 0;
+      stack<int> st1, st2;
         
-      for(char it: s){
-          
-          if(it == '('){
-              hi++;
-              lo++;
+      for(int i = 0; i < s.length(); i++){
+          if(s[i] == '('){
+              st1.push(i);
           }
-          else if(it == ')'){
-              lo = max(0, --lo);
-              hi--;
+          else if(s[i] == '*'){
+              st2.push(i);
           }
-          else if(it == '*'){
-              lo = max(0, --lo);
-              hi++;
-          }
-          if(hi < 0){
-              return false;
+          else{
+              if(!st1.empty()){
+                  st1.pop();
+              }
+              else if(!st2.empty()){
+                  st2.pop();
+                  
+              }
+              else
+                  return false;
           }
       }
         
-        return lo == 0;
+        while(!st1.empty() && !st2.empty()){
+            if(st1.top() > st2.top())
+                return false;
+            
+            st1.pop();
+            st2.pop();
+        }
+        return st1.empty();
     }
 };
