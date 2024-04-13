8b8b8104d8e39189a5fc893d0d4330f2f722e20f
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2024-04-13 22:10:22 +0530

    Time: 33 ms (55.46%), Space: 17 MB (86.54%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-         if (matrix.empty() || matrix[0].empty()) return 0;
-
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-
-    std::vector<int> heights(cols, 0);
-    int maxArea = 0;
-
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            if (matrix[i][j] == '1') {
-                heights[j]++;
-            } else {
-                heights[j] = 0;
-            }
-        }
         
-        std::stack<int> stk;
-        for (int j = 0; j <= cols; ++j) {
-            while (!stk.empty() && (j == cols || heights[j] < heights[stk.top()])) {
-                int height = heights[stk.top()];
-                stk.pop();
-                int width = stk.empty() ? j : j - stk.top() - 1;
-                maxArea = std::max(maxArea, height * width);
+     int n = matrix.size();
+     int m = matrix[0].size();
+        
+     if(matrix.empty() || matrix[0].empty())
+         return 0;
+    
+     vector<int> v(m, 0);
+     int maxi = 0;
+
+     for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < m; ++j) {
+            if(matrix[i][j] == '1') {
+                v[j]++;
+            } 
+            else{
+                v[j] = 0;
             }
-            stk.push(j);
+         }
+        
+      stack<int> st;
+         
+      for(int j = 0; j <= m; ++j) {
+        while(!st.empty() && (j == m || v[j] < v[st.top()])) {
+                int height = v[st.top()];
+                st.pop();
+                int width = st.empty() ? j : j - st.top() - 1;
+                maxi= max(maxi, height * width);
+            }
+            st.push(j);
         }
     }
 
-    return maxArea;
+    return maxi;
     }
 };
